bdeb8ceaa21495f0e09b2c5004ac84aba52f456c
dpalmasan <dpalmasan@gmail.com>
2020-03-16 22:19:35 -0300

    Add needed configuration files for CI/CD

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup script for TRUNAJOD.
+
+This is the setup.py script for TRUNAJOD, to build and package TRUNAJOD.
+"""
+from distutils.core import setup
+
+setup(
+    name='TRUNAJOD',
+    version='1.0',
+    description='TRUNAJOD readability utilities',
+    author='Diego Palma',
+    author_email='dipalma@udec.cl',
+    packages=['TRUNAJOD'])
