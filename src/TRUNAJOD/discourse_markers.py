c33dbad5c3a8c274e834f85629ec9691a1382cca
dpalmasan <dpalmasan@gmail.com>
2020-04-10 19:50:14 -0300

    Update discourse markers docs.

--- src/TRUNAJOD/discourse_markers.py
+++ src/TRUNAJOD/discourse_markers.py
@@ -1,4 +1,55 @@
-"""TRUNAJOD discourse markers module."""
+"""TRUNAJOD discourse markers module.
+
+This discourse markers module is based on the lexicon described in
+:cite:`i2005representing`, and at the point of writing they are under
+https://cs.famaf.unc.edu.ar/~laura/shallowdisc4summ/discmar/. TRUNAJOD
+comes with this lexicon bundled, and this module has the following set
+'constants':
+
++-------------------------------+---------------------------------------------+
+| Type of discourse marker      | Discourse marker                            |
++===============================+=============================================+
+| cause                         | dado que, porque, debido a, gracias a,      |
+|                               | por si, por, por eso, en conclusión,        |
+|                               | así que, como consecuencia, para, para que, |
+|                               | por estar razón, por tanto, en efecto       |
++-------------------------------+---------------------------------------------+
+| context                       | teniendo en cuenta, después, antes,         |
+|                               | originalmente, a condición de, durante,     |
+|                               | mientras, a no ser que, cuando, donde,      |
+|                               | de acuerdo con, lejos de, tan pronto como,  |
+|                               | por el momento, entre, hacia, hasta,        |
+|                               | mediante, según, en cualquier caso,         |
+|                               | entonces, respecto a, en ese caso, si,      |
+|                               | siempre que, sin duda, a la vez             |
++-------------------------------+---------------------------------------------+
+| equality                      | en resumen, concretamente, en esencia,      |
+|                               | en comparación, en otras palabras,          |
+|                               | en particular, es decir, por ejemplo,       |
+|                               | precisamente, tal como, por último,         |
+|                               | por un lado, por otro lado, a propósito,    |
+|                               | no sólo, sino también, en dos palabras,     |
+|                               | además, también, aparte, aún es más,        |
+|                               | incluso, especialmente, sobretodo           |
++-------------------------------+---------------------------------------------+
+| highly polysemic              | como, desde, sobre, antes que nada,         |
+|                               | para empezar                                |
++-------------------------------+---------------------------------------------+
+| Revision                      | a pesar de, aunque, excepto, pese a,        |
+|                               | no obstante, sin embargo, en realidad,      |
+|                               | de hecho, al contrario, el hecho es que,    |
+|                               | es cierto que, pero', con todo, ahora bien, |
+|                               | de todos modos                              |
++-------------------------------+---------------------------------------------+
+| Vague meaning closed class    | y, e, ni, o, u, que, con, sin, contra,      |
+| words                         | en, a,                                      |
++-------------------------------+---------------------------------------------+
+
+The following module constants are defined as sets ``CAUSE_DISCOURSE_MARKERS``,
+``CONTEXT_DISCOURSE_MARKERS``, ``EQUALITY_DISCOURSE_MARKERS``,
+``HIGHLY_POLYSEMIC_DISCOURSE_MARKERS``, ``REVISION_DISCOURSE_MARKERS``,
+``VAGUE_MEANING_CLOSED_CLASS_WORDS``.
+"""
 import re
 
 CAUSE_DISCOURSE_MARKERS = {
@@ -121,7 +172,13 @@ def find_matches(text, list):
     """Return matches of words in list in a target text.
 
     Given a text and a list of possible matches (in this module, discourse
-    markers list), returns the number of matches found in text.
+    markers list), returns the number of matches found in text. This ignores
+    case.
+
+    .. hint:: For non-Spanish users
+       You could use this function with your custom list of discourse markers
+       in case you need to compute this metric. In that case, the way to call
+       the funcion would be: ``find_matches(YOUR_TEXT, ["dm1", "dm2", etc])``
 
     :param text: Text to be processed
     :type text: string
