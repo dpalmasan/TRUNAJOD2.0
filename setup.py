7407f90a8f18a61368d5bc50b157b0cb667430b5
dpalmasan <dpalmasan@gmail.com>
2020-04-11 02:56:25 -0300

    Fixing setup and adding MANIFEST.in

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ Setup script for TRUNAJOD.
 This is the setup.py script for TRUNAJOD, to build and package TRUNAJOD.
 """
 from distutils.core import setup
-from os import path
-
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
     name='TRUNAJOD',
-    version='0.1.5',
+    version='0.1.0',
     license='MIT',
     description="A python lib for readability analyses.",
-    long_description_content_type='text/markdown',
     long_description=open('README_pypi.rst').read(),
     author='Diego Palma',
     author_email='dipalma@udec.cl',
