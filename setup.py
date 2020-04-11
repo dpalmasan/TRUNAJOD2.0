6a1bc328c9034c59ef865d27f63fc64ac2d3eb4a
dpalmasan <dpalmasan@gmail.com>
2020-04-11 02:44:17 -0300

    Update setup file.

--- setup.py
+++ setup.py
@@ -4,21 +4,25 @@ Setup script for TRUNAJOD.
 This is the setup.py script for TRUNAJOD, to build and package TRUNAJOD.
 """
 from distutils.core import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='TRUNAJOD',
-    version='0.1',
+    version='0.1.5',
     license='MIT',
-    description='TRUNAJOD readability library for Python',
+    description="A python lib for readability analyses.",
+    long_description_content_type='text/markdown',
+    long_description=open('README_pypi.rst').read(),
     author='Diego Palma',
     author_email='dipalma@udec.cl',
     url="https://github.com/dpalmasan/TRUNAJOD2.0",
     download_url=(
         "https://github.com/dpalmasan/TRUNAJOD2.0/archive/v0.1.tar.gz"),
-    keywords=[
-        "NLP", "readability", "entity grid", "linguistics", "computational",
-        "spacy"
-    ],
+    keywords=["NLP", "readability", "entity grid", "linguistics"],
     packages=['TRUNAJOD'],
     package_dir={'': 'src'},
     classifiers=[
